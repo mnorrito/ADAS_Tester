050018b8c36c248fe141e180cfc266c9380f2028
Mathieu Norrito <mathieu.norrito@gmail.com>
2017-04-07 09:49:19 +0200

    New .gitignore with .meta included in project

---
+++ Assets/SampleScenes/Python/detect_pedestrian.m
@@ -0,0 +1,13 @@
+function [a, b] = detect_pedestrian(distanceToWalker, throttle, speed)
+    pedestrian = 0.0
+    
+    if (distanceToWalker < 15)
+        throttle = -1.0
+        pedestrian = 1.0
+        if (speed < 1)
+            throttle = 0.0
+        end
+    end
+    a = throttle
+    b = pedestrian
+end
