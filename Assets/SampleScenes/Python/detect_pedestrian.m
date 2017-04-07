18d39c46ed0e196338080432e7ebd1aa34742bb9
Mathieu Norrito <mathieu.norrito@gmail.com>
2017-04-07 14:09:55 +0200

    First step of modification on LakeRoad scene

--- Assets/SampleScenes/Python/detect_pedestrian.m
+++ Assets/SampleScenes/Python/detect_pedestrian.m
@@ -1,10 +1,10 @@
 function [a, b] = detect_pedestrian(distanceToWalker, throttle, speed)
     pedestrian = 0.0
     
-    if (distanceToWalker < 15)
+    if (distanceToWalker < 18)
         throttle = -1.0
         pedestrian = 1.0
-        if (speed < 1)
+        if (speed < 1.5)
             throttle = 0.0
         end
     end
